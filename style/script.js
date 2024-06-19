acd92cf4aaadffecc182fec990280c28467d100a
mohameduxv <elkmohamed77@gmail.com>
2024-06-19 21:06:11 +0100

    Update script.js

--- style/script.js
+++ style/script.js
@@ -195,3 +195,17 @@ function updateProgressLine() {
         ProgressLine.style.width = `100%`; // Handle max level case
     }
 }
+function Copy() {
+    // Get the text field
+    var copyText = document.getElementById("ref-link");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  }
